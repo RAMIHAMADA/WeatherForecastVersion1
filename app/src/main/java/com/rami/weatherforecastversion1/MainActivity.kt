da3e2a96ae1ac01c8962dbe0a265f39d66b9fa0f
Khamadekh Rami <khamadekh@gmail.com>
2023-09-21 18:49:07 +0300

    checking api for information

--- app/src/main/java/com/rami/weatherforecastversion1/MainActivity.kt
+++ app/src/main/java/com/rami/weatherforecastversion1/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.rami.weatherforecastversion1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.rami.weatherforecastversion1.databinding.ActivityMainBinding
+import org.json.JSONObject
+
+const val API_KEY = "43a8ac71df6347548d1144132232109"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -10,5 +16,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.bGet.setOnClickListener {
+            getResult("London")
+        }
+    }
+
+    private fun getResult(name: String) {
+        val url = "https://api.weatherapi.com/v1/current.json?key=$API_KEY&q=$name&aqi=no"
+
+        val queue = Volley.newRequestQueue(this)
+        val stringRequest = StringRequest(
+            com.android.volley.Request.Method.GET,
+            url,
+            { response ->
+                val obj = JSONObject(response)
+                val temp = obj.getJSONObject("current")
+                Log.e("COK", "Response: ${temp.getString("temp_c")}")
+            },
+            {
+                Log.e("COK", "Volley error: $it")
+            },
+        )
+        queue.add(stringRequest)
     }
 }
