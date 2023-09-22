6006689814cb67ca431fe2d7314033bd4ff2a78a
Khamadekh Rami <khamadekh@gmail.com>
2023-09-22 16:08:33 +0300

    added a permission check to the user's location

--- app/src/main/java/com/rami/weatherforecastversion1/fragments/MainFragment.kt
+++ app/src/main/java/com/rami/weatherforecastversion1/fragments/MainFragment.kt
@@ -1,13 +1,18 @@
 package com.rami.weatherforecastversion1.fragments
 
+import android.Manifest
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import com.rami.weatherforecastversion1.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
+    private lateinit var pLauncher: ActivityResultLauncher<String>
     private lateinit var binding: FragmentMainBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,7 +22,26 @@ class MainFragment : Fragment() {
         return binding.root
     }
 
-    companion object{
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        checkPermissions()
+    }
+
+    private fun permissionListener() {
+        pLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            Toast.makeText(activity, "Permissions is  $it", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun checkPermissions(){
+        if (!isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)){
+            permissionListener()
+            pLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
+
+
+    companion object {
         @JvmStatic
         fun newInstance() = MainFragment()
     }
