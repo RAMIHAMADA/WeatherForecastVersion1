3ce42dd4effff91e12eaf8755e3502a3f99c998c
Khamadekh Rami <khamadekh@gmail.com>
2023-09-27 15:15:48 +0300

    added recyclerView in fragment_hours and added new Adapter + init in HoursFragment

--- app/src/main/java/com/rami/weatherforecastversion1/fragments/HoursFragment.kt
+++ app/src/main/java/com/rami/weatherforecastversion1/fragments/HoursFragment.kt
@@ -5,16 +5,48 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.rami.weatherforecastversion1.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.rami.weatherforecastversion1.adapters.WeatherAdapter
+import com.rami.weatherforecastversion1.data.WeatherModel
+import com.rami.weatherforecastversion1.databinding.FragmentHoursBinding
 
 
 class HoursFragment : Fragment() {
+    private lateinit var binding: FragmentHoursBinding
+    private lateinit var adapter: WeatherAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hours, container, false)
+        binding = FragmentHoursBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRecycleView()
+    }
+
+    private fun initRecycleView() = with(binding) {
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+        adapter = WeatherAdapter()
+        recyclerView.adapter = adapter
+        val list = listOf(
+            WeatherModel(
+                "", "12:00", "Sunny", "25C",
+                "", "", "", ""
+            ),
+            WeatherModel(
+                "", "13:00", "Sunny", "20C",
+                "", "", "", ""
+            ),
+            WeatherModel(
+                "", "14:00", "Sunny", "15C",
+                "", "", "", ""
+            )
+        )
+
+        adapter.submitList(list)
     }
 
     companion object {
